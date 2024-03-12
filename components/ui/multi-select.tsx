854892eecdb9a0ba840f5ebe746a033b53557c90
yousef69mr <youseffouad69@gmail.com>
2024-03-12 19:15:12 +0200

    0.3.3 version fix some build errors

--- components/ui/multi-select.tsx
+++ components/ui/multi-select.tsx
@@ -88,8 +88,8 @@ const MultiSelect = React.forwardRef<HTMLInputElement, InputProps>(
             </FormControl>
           </PopoverTrigger>
           <div className="w-full gap-6 items-center justify-start">
-            {selectedOptions.map((option) => (
-              <Badge>
+            {selectedOptions.map((option,index) => (
+              <Badge key={index}>
                 {option.label}{" "}
                 <X
                   onClick={() =>
@@ -140,36 +140,6 @@ const MultiSelect = React.forwardRef<HTMLInputElement, InputProps>(
           </div>
         </DrawerContent>
       </Drawer>
-    );
-    return (
-      <>
-        <motion.div
-          style={{
-            background: useMotionTemplate`
-      radial-gradient(
-        ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
-        var(--blue-500),
-        transparent 80%
-      )
-    `,
-          }}
-          onMouseMove={handleMouseMove}
-          onMouseEnter={() => setVisible(true)}
-          onMouseLeave={() => setVisible(false)}
-          className="p-[2px] rounded-lg transition duration-300 group/input"
-        >
-          <input
-            type={type}
-            className={cn(
-              "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-300",
-              className
-            )}
-            ref={ref}
-            {...props}
-          />
-        </motion.div>
-        <motion.div className="flex w-full gap-6 flex-wrap items-center"></motion.div>
-      </>
     );
   }
 );
